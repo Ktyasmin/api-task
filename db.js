decc3a268c75ccd10c17092be34eeae506ec970e
ktyasmin <ladeedayasmin881.com>
2025-12-09 12:03:51 +0530

    Ready for deployment

--- db.js
+++ db.js
@@ -21,18 +21,15 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,       // Use Render environment variable
-    user: process.env.DB_USER,       // Use Render environment variable
-    password: process.env.DB_PASSWORD, // Use Render environment variable
-    database: process.env.DB_NAME    // Use Render environment variable
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.log("Error connecting to MySQL:", err);
-        return;
-    }
-    console.log("MySQL Connected!");
+connection.connect(err => {
+    if (err) console.error("Error connecting to MySQL:", err);
+    else console.log("MySQL Connected!");
 });
 
 module.exports = connection;
